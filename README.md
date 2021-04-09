5880dea90bebd1c53609da67e4bb6d4ee5c71251
Sam Schmitt <sam@samschmitt.com>
2021-04-09 18:21:22 -0400

    Playing around with directories

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ansible Scaffold
 A CLI tool to build out Ansible roles easily. Built with Rust.
+
+## Usage
+
+`ansible-scaffold layout.toml`
+
--- ansible-scaffold/src/main.rs
+++ ansible-scaffold/src/main.rs
@@ -2,6 +2,14 @@ use std::fs;
 extern crate clap;
 use clap::{Arg, App, SubCommand};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let projectName: String;
+
+    fs::create_dir("/hello/word")?;
+
+
+}
+
+struct Role {
+    name: String,
 }
